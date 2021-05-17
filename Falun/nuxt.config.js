7dfd19edde7d0155b51bb032cb56c89df3e1dabc
PitBosch <pietro.boschini@mail.polimi.it>
2021-05-17 19:18:36 +0200

    areas changed with request to api + added another area

--- Falun/nuxt.config.js
+++ Falun/nuxt.config.js
@@ -36,7 +36,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [{src: "~/plugins/vue2-google-maps.js"}],
+  plugins: [{ src: '~/plugins/vue2-google-maps.js' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -58,6 +58,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    vendor: ["vue2-google-maps"]
+    vendor: ['vue2-google-maps'],
   },
 }
